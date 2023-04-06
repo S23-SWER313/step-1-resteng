ea4841ccc8fca0dcd4b3216182909791d77d4fc3
tis <abusalhm106@gmail.com>
2023-04-06 22:34:00 +0300

    init project

---
+++ resteng/src/test/java/com/resteng/resteng/RestengApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resteng.resteng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestengApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
