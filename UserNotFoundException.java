2160ef945d06fb93fbee25300b5b99dc98ba8b90
waseemABuobeid <102429086+Zer0o0o0@users.noreply.github.com>
2023-04-27 21:19:23 +0300

    Delete UserNotFoundException.java

--- UserNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.osama.rest;
-
-public class UserNotFoundException extends RuntimeException{
-    UserNotFoundException(Integer id) {
-        super("Could not find employee " + id);
-      }
-
-}
